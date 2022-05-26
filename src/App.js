4a20637e69dca4545aa44b3113603fd93da5d6b1
Rohit Kumar <rohit16546@gmail.com>
2022-05-27 02:59:03 +0530

    some changes regarding viewport

--- src/App.js
+++ src/App.js
@@ -38,28 +38,11 @@ function App() {
 
   return (
     <div className="App">
-      {/* <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <p>This will Also work!</p>
-        </header>
-      </div> */}
-      <div className="App-header">
-        <NewExpense onAddExpense={addExpenseHandler} />
-        <Expenses items={expenses} />
-      </div>
+      {/* <div className="App-header"> */}
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
+    // </div>
   );
 }
 
